74b1a555cb4aef675e647e468ada1c59f4977da8
AwwalMudashir <awwalmudashir2@gmail.com>
2025-02-05 19:01:46 +0100

    Update Vite config to fix react-d3-tree import issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['react-d3-tree'], // Include react-d3-tree in optimized dependencies
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-d3-tree'], // Externalize react-d3-tree in production build
+    },
+  },
+});
